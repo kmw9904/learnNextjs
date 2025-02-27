a115826453ee4bd6944c6c8d6ab5940b52f78ca5
kmw <skqjahjakskxl@gmail.com>
2025-02-27 19:26:18 +0900

    페이지별 레이아웃 설정

--- section02/src/pages/search/index.tsx
+++ section02/src/pages/search/index.tsx
@@ -1,4 +1,6 @@
 import { useRouter } from "next/router";
+import { ReactNode } from "react";
+import SearchableLayout from "../components/searchable-layout";
 
 export default function Page() {
   const router = useRouter();
@@ -6,3 +8,7 @@ export default function Page() {
   const q = router.query.q;
   return <h1>search {q}</h1>;
 }
+
+Page.getLayout = (page: ReactNode) => {
+  return <SearchableLayout>{page}</SearchableLayout>;
+};
